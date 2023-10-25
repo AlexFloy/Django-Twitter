9628d89f86ae26e27e9b72b971853751881dd53c
AlexFloy <rusanov0111@gmail.com>
2023-10-25 14:20:25 +0300

    add urls from user_name

--- mini_twitter/urls.py
+++ mini_twitter/urls.py
@@ -19,13 +19,13 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
+from posts.views import posts_menu
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include("users.urls")),
     path('posts/', include("posts.urls")),
-
 ]
 
 if settings.DEBUG:
